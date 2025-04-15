54a68eb47f60c6af8b9313285f2d0ee391bf8506
childressdev <jcampbell@childressagency.com>
2025-04-15 16:48:39 -0400

    header styled

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -5,3 +5,13 @@
 jQuery(function($){
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) {
+      document.getElementById('header').classList.add('header-scrolled');
+    } else {
+      document.getElementById('header').classList.remove('header-scrolled');
+    }
+  });
+});
